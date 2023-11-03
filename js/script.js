538d1e78b2a7d17b4f79a46a3451a1533d5ebd6f
Wishmi Tharuka <92284830+vishmitharuka57@users.noreply.github.com>
2023-11-03 12:21:22 +0530

    initialized

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let navbar = document.querySelector('.navbar')
+
+document.query
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+   /* loginForm.classList.remove('active');
+    searchForm.classList.remove('active');*/
+}
+
+let loginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+     searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+
+}
+
+window.onscroll =() =>{
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if ( themeBtn.classList.toggle('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+};
+
